cc5bd02315a75fdb7b5a557b29c926c0d5b37833
Sean E. Russell <ser@ser1.net>
2020-04-29 09:52:28 -0500

    Only one shorthand-only flag was allowed (bug)

--- flag.go
+++ flag.go
@@ -434,7 +434,9 @@ func (f *FlagSet) VarP(value Value, name, shorthand, usage string) {
 	if name == "" {
 		flag.shorthandOnly = true
 		flag.Name = flag.Shorthand
+		name = shorthand
 	}
+	fmt.Printf("NAME = %s\n", name)
 	_, alreadythere := f.formal[name]
 	if alreadythere {
 		msg := fmt.Sprintf("%s flag redefined: %s", f.name, name)
